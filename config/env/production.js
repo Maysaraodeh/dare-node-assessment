e22201b9a54fa2d6026edd88185efa4c7353a29d
maysaraodeh <maso-505@hotmail.com>
2020-07-23 00:49:08 +0200

    Add env variables
    new env variables for secrets and clients

--- config/env/production.js
+++ config/env/production.js
@@ -4,5 +4,7 @@ export default {
   CORS_ALLOWED_LINK: '*',
   jwt_encryption: 'wxI[0c733Lo<4>B4*!MW+BOHOIXm~Z',
   jwt_expiration: '8h',
-  INSURANCE_API_BASE_RUL: 'https://dare-nodejs-assessment.herokuapp.com/api',
+  INSURANCE_API_BASE_URL: 'https://dare-nodejs-assessment.herokuapp.com/api',
+  INSURANCE_API_CLIENT_ID: 'axa',
+  INSURANCE_API_CLIENT_SECRET: 's3cr3t',
 };
