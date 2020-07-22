76bbbfc96f656882733bceb069a3aec61421cde2
maysaraodeh <maso-505@hotmail.com>
2020-07-22 20:47:41 +0200

    Add middlewares
    request payload validators

---
+++ middlewares/validate.js
@@ -0,0 +1,17 @@
+import joi from 'joi';
+import httpResponse from '../helpers/httpResponse';
+
+export const validate = (validateCheck) => (req, res, next) => {
+  const expectedKeys = Object.keys(validateCheck);
+  const objectValidate = expectedKeys.reduce((accum, curr) => {
+    accum[curr] = req[curr];
+    return accum;
+  }, {});
+  const validateStatus = joi.validate(objectValidate, validateCheck);
+  if (validateStatus.error)
+    httpResponse.badRequest(
+      res,
+      validateStatus.error.details[0].message.replace(/"/g, '')
+    );
+  else next();
+};
