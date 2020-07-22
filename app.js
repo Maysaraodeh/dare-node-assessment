51859ffb2fe61c452af7289a3d807f81736bc4b4
maysaraodeh <maso-505@hotmail.com>
2020-07-22 20:38:01 +0200

    Initialize server
    server initialization and running

---
+++ app.js
@@ -0,0 +1,44 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import passport from 'passport';
+import path from 'path';
+import config from './config';
+
+const { corsConfig, CORS_ALLOWED_LINK } = config;
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', CORS_ALLOWED_LINK);
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
+app.use(cors(corsConfig));
+app.use(cookieParser());
+
+module.exports = app;
