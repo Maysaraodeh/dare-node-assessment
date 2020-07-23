2e94d5f7409d0199e7c6b3d68de1be0fcdfbcf5b
maysaraodeh <maso-505@hotmail.com>
2020-07-23 13:30:59 +0200

    Modify helpers and packages
    modification to match the required results

--- helpers/utils.js
+++ helpers/utils.js
@@ -3,3 +3,12 @@ import moment from 'moment';
 export const getExpirationTime = ({ expires = moment() }) => {
   return moment(expires).utc().unix() - moment().utc().unix();
 };
+
+export const arrayToEnum = (arr) => {
+  const obj = {};
+  arr.forEach((value, index) => {
+    obj[value] = index + 1;
+    obj[index + 1] = value;
+  });
+  return obj;
+};
