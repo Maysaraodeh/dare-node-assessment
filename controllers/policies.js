b7bda0092a1d44b2795bb5719dcb11c85cdeb9d6
maysaraodeh <maso-505@hotmail.com>
2020-07-24 00:33:06 +0200

    Add policy controller
    controle policies flow and connect with the services

---
+++ controllers/policies.js
@@ -0,0 +1,31 @@
+import httpResponse from '../helpers/httpResponse';
+import { userRolesEnums } from '../middlewares/enum/users';
+import { property } from '../helpers/propertiesReader';
+import {
+  findPoliciesByFilter,
+  findAllPolicies,
+  findPolicy,
+} from '../services/policies';
+
+export const getPolicies = async (req, res) => {
+  const { user, query } = req;
+  let result = [];
+  if (user.role === userRolesEnums[userRolesEnums.user])
+    result = await findPoliciesByFilter({ filed: 'clientId', value: user.id });
+  else result = await findAllPolicies(query);
+  return httpResponse.ok(res, result);
+};
+
+export const getPolicy = async (req, res) => {
+  const {
+    user,
+    params: { id: policyId },
+  } = req;
+  let policy = await findPolicy({ field: 'id', value: policyId });
+  if (user.role === userRolesEnums[userRolesEnums.admin]) {
+    return httpResponse.ok(res, policy);
+  }
+  if (policy.clientId !== user.id)
+    return httpResponse.unAuthorized(res, property('clients.unAuthorized'));
+  return httpResponse.ok(res, policy);
+};
