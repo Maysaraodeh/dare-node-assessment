9015110d46b56e466229d3f8478f38b70045c44f
maysaraodeh <maso-505@hotmail.com>
2020-07-23 13:35:02 +0200

    Add endpoint test
    testing login endpoint routes

---
+++ test/unit/clients/data.js
@@ -0,0 +1,23 @@
+export const clientsArray = [
+  {
+    id: 'a0ece5db-4f21-812f-966633e7be86',
+    name: 'Britney',
+    email: 'britne@quotezart.com',
+    role: 'admin',
+    policies: [],
+  },
+  {
+    id: 'a0ece5-4f21-812f-966633e7be86',
+    name: 'Brney',
+    email: 'britnnkenship@quotezart.com',
+    role: 'admin',
+    policies: [],
+  },
+  {
+    id: 'a0ece5db-cd14-4f21-812f-966633e7be86',
+    name: 'Britney',
+    email: 'britneyblankenship@quotezart.com',
+    role: 'admin',
+    policies: [],
+  },
+];
