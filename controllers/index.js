41360221f8253bcbd3a98e2bd80b624ef9ae0254
maysaraodeh <maso-505@hotmail.com>
2020-07-24 16:09:02 +0200

    Add clients controller
    getClients,getClient,getClientPolicies

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,6 @@
 import * as users from './users';
 import * as policies from './policies';
+import * as clients from './clients';
 export const userController = users;
 export const policiesController = policies;
+export const clientsController = clients;
