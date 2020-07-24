b5fc6b8270f44646a154ceea3701c3c98087c9bc
maysaraodeh <maso-505@hotmail.com>
2020-07-24 16:16:01 +0200

    Add new service
    add findById service to users, and added static password

--- services/users.js
+++ services/users.js
@@ -3,7 +3,11 @@ export const findUserByEmail = (email) => {
   return findClientByFilter({ field: 'email', value: email });
 };
 
+export const findUserById = (id) => {
+  return findClientByFilter({ field: 'id', value: id });
+};
+
 export const compareUserPassword = (user, password) => {
   // we don't have any passwords assigned to the clients
-  return true;
+  return password === 'abcd1234';
 };
