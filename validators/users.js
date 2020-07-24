eb12dfd50d8f80213232bcbdca8d537be7dda961
maysaraodeh <maso-505@hotmail.com>
2020-07-24 18:19:50 +0200

    Fixed user validation
    add capital letters to username validation

--- validators/users.js
+++ validators/users.js
@@ -1,5 +1,5 @@
 import joi from '@hapi/joi';
-const validUsernameRegex = /^[a-z0-9_-]{3,16}$/;
+const validUsernameRegex = /^[A-Za-z0-9_-]{3,16}$/;
 export const loginValidation = {
   body: {
     username: joi.string().required().regex(validUsernameRegex),
