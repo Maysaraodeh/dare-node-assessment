d65352f33d3f099deb81dc5edf1f8810fa3f0e57
maysaraodeh <maso-505@hotmail.com>
2020-07-24 16:08:09 +0200

    Add clients routes
    get /clients, /clients/:id, /clients/:id/policies

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import usersRoutes from './users';
 import policiesRoutes from './policies';
+import clientsRoutes from './clients';
 export const router = express.Router();
 
 router.use('/', usersRoutes);
 router.use('/policies', policiesRoutes);
+router.use('/clients', clientsRoutes);
