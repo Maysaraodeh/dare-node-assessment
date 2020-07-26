412064af6ed3ecd383af9f779892b4b24367a3dc
maysaraodeh <maso-505@hotmail.com>
2020-07-26 15:35:42 +0200

    Add dotenv
    to manage env variables without exposing the secrets

--- config/env/development.js
+++ config/env/development.js
@@ -2,9 +2,5 @@ export default {
   port: 5001,
   corsConfig: '*',
   CORS_ALLOWED_LINK: '*',
-  JWT_ENCRYPTION: 'wxI[0c733Lo<4>B4*!MW+BOHOIXm~Z',
   JWT_EXPIRATION: '24h',
-  INSURANCE_API_BASE_URL: 'https://dare-nodejs-assessment.herokuapp.com/api',
-  INSURANCE_API_CLIENT_ID: 'axa',
-  INSURANCE_API_CLIENT_SECRET: 's3cr3t',
 };
